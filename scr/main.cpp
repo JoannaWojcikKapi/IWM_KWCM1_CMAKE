db6cd31d77e7ab8356266d99b29b8bb575022919
kapibara <asia9215@gmail.com>
2018-06-17 15:46:54 +0200

    3.4 controll compilation option- option on

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,2 +1,25 @@
+#include <LaborkaConfig.h>
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include <trygonometria.h>
+#else
+#include <cmath>
+#endif
 int main()
-{ /*...*/ return 0; }
+{ 
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+double degree = 34;
+double result = degreemath::cos_degree(degree);
+double result1 = degreemath::sin_degree(degree);
+double result2 = degreemath::tg_degree(degree);
+double result3 = degreemath::ctg_degree(degree);
+#else
+double rad = 4.0;
+double pi = 3.14;
+double result = cos(pi/rad);
+double result1 = sin(pi/rad);
+double result2 = tan(pi/rad);
+double result3 = 1/tan(pi/rad);
+#endif
+ return 0;
+}
